7a328f1a03931fbf8c4e9297fe952a86e81f5f53
Abel Alonso <abel.alonso@gmail.com>
2018-10-23 20:35:27 +0200

    testing

---
+++ src/app/1-voter/voter.component.spec.ts
@@ -0,0 +1,40 @@
+import { VoterComponent } from './voter.component';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { componentFactoryName } from '@angular/compiler';
+import { By } from '@angular/platform-browser';
+
+describe('VoterComponent', () => {
+  let component: VoterComponent;
+  let fixture: ComponentFixture<VoterComponent>;
+
+    beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ VoterComponent ]
+    });
+
+    fixture = TestBed.createComponent(VoterComponent);
+    component = fixture.componentInstance;
+    // fixture.nativeElement returns the HTML component (root DOM)
+    // fixture.debugElement
+  });
+
+  it('shold render total votes', () => {
+    component.othersVote = 20;
+    component.myVote = 1;
+    fixture.detectChanges();
+    const de = fixture.debugElement.query(By.css('.vote-count'));
+    const el: HTMLElement = de.nativeElement;
+    expect(el.innerText).toContain('21');
+  });
+  it('should highlight the upvote button if I have upvoted', () => {
+    component.myVote = 1;
+    fixture.detectChanges();
+    const de = fixture.debugElement.query(By.css('.glyphicon-menu-up'));
+    expect(de.classes['highlighted']).toBeTruthy();
+  });
+  it('should increase totalVOtes when I click upvote button', () => {
+    const button = fixture.debugElement.query(By.css('.glyphicon-menu-up'));
+    button.triggerEventHandler('click', null);
+    expect(component.totalVotes).toBe(1);
+  });
+});
---
+++ src/app/highlight.directive.ts
@@ -0,0 +1,17 @@
+import { Directive, Input, ElementRef, OnChanges } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]'
+})
+export class HighlightDirective implements OnChanges {
+  defaultColor =  'yellow';
+  // tslint:disable-next-line:no-input-rename
+  @Input('highlight') bgColor: string;
+
+  constructor(private el: ElementRef) {
+  }
+
+  ngOnChanges() {
+    this.el.nativeElement.style.backgroundColor = this.bgColor || this.defaultColor;
+  }
+}
